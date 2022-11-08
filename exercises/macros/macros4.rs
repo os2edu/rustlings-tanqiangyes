37e2d9095473f9b5bfc54e1cbcf0888900e0670c
tanqiangyes <826285820@qq.com>
2022-11-08 14:07:43 +0800

    macros, standard, threads

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -1,13 +1,12 @@
 // macros4.rs
 // Execute `rustlings hint macros4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
+#[macro_export]
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    }
-    ($val:expr) => {
+    };
+    ($val: expr) => {
         println!("Look at this other macro: {}", $val);
     }
 }
